30f1c9e0fa0ab55f32429f9ead92a7ecb4e86ffe
username <email>
2025-07-25 23:53:34 +0530

    stack skills

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 import express from 'express';
 import { config } from 'dotenv';
 import databaseConnection from './database/dbConnection.js';
+import UserModel from './models/user.model.js';
 config();
 
 const app = express();
+app.use(express.json());
 
 const BACKEND_PORT = process.env.PORT
+
+// register user
+
+app.post("/register", async (req, res) => {
+  const username = req.body.name   // json
+  const useremail = req.body.email
+  const userpassword = req.body.password
+
+  await UserModel.create({
+    name: username,
+    email: useremail,
+    password: userpassword
+  })
+
+  res.send("Registration successfull")
+})
 
 app.listen(BACKEND_PORT, async () => {
   await databaseConnection();
