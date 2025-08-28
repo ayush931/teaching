422918b809c76795dfd82ff3d7b87d5147fd997b
username <email>
2025-08-29 00:57:08 +0530

    models

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,30 @@
 import User from "../models/user.model.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+// 1. create token -> tells that u r logged in or not
+// 2. send it through cookie -> helps to set the token into the system
+// 3. hash the password
+
+const JWT_PASSWORD = "12345";
+
+const token = jwt.sign({
+  id: createUser._id,
+  email: createUser.email
+  }, {
+    JWT_PASSWORD
+  }, {
+    expiresIn: "7d"
+})
+
+const cookieOptions = {
+  secure: true,
+  httpOnly: true,
+  maxAge: 7 * 24 * 60 * 60 * 1000
+}
 
 export const registerController = async (req, res) => {
   const { name, email, password, phone } = req.body;
-
-  // success -> false -> frontend;
 
   if (!name || !email || !password || !phone) {
     return res.status(400).json({
@@ -21,12 +42,16 @@ export const registerController = async (req, res) => {
     })
   }
 
+  const hashPassword = bcrypt.hash(password, 10);
+
   const createUser = await User.create({
     name,
     email,
-    password,
+    password: hashPassword,
     phone
   })
+
+  res.cookie("token", token, cookieOptions);
 
   await createUser.save();
 
@@ -47,7 +72,7 @@ export const loginController = async (req, res) => {
     })
   }
 
-  const findUser = await User.findOne({ email });
+  const findUser = await User.findOne({ email }).select("+password");
 
   if (!findUser) {
     return res.status(400).json({
@@ -56,18 +81,16 @@ export const loginController = async (req, res) => {
     })
   }
 
-  // findUser.password -> database ka password
-  // password -> password jo user diya
-  // dono ko match krega
-  // compare ho jaate hai -> true
+  const comparePassword = bcrypt.compare(findUser.password, password);
 
-  const comparePassword = password === findUser.password; // true / false
   if (!comparePassword) {
     return res.status(400).json({
       success: false,
       message: 'Invalid credentials'
     })
   }
+
+  res.cookie("token", token, cookieOptions);
 
   return res.status(200).json({
     success: true,
--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import { config } from 'dotenv';
 import connectionToDB from './database/dbConnection.js';
 import userRouter from './routes/user.route.js';
+import cookieParser from 'cookie-parser';
 config();
 
 const PORT = process.env.PORT;
+// cookie is a small piece of data sent from a website and saved on your device, which helps keeping you logged in,
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 app.use('/user', userRouter);
 
 // localhost:4000/user/register
